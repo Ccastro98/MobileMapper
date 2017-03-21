d888cf166d88657cf45a6f8097ca45eece67f955
student5 <student3@jh-mba158-04.d214.org>
2017-03-21 10:02:18 -0500

    Find location with just name

--- MobileMapperLecture/ViewController.swift
+++ MobileMapperLecture/ViewController.swift
@@ -11,18 +11,42 @@ import MapKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
+    
+    let herseyAnnotation = MKPointAnnotation()
+    
+    let address = "University of Illinois"
+    let geocoder = CLGeocoder()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let herseyAnnotation = MKPointAnnotation()
-        let latitude: Double = 42.102332924
-        let longitude: Double = -87.955667844
-        let cordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-        herseyAnnotation.coordinate = cordinate
+        
+//        let latitude: Double = 42.102332924
+//        let longitude: Double = -87.955667844
+//        let cordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        let cordinateTwo = CLLocationCoordinate2D(latitude: 42.102332924, longitude: -87.955667844)
+        herseyAnnotation.coordinate = cordinateTwo
         herseyAnnotation.title = "John Hersey High School"
-        mapView.addAnnotation(herseyAnnotation) 
+        mapView.addAnnotation(herseyAnnotation)
+        
+       
+        geocoder.geocodeAddressString(address) { (placemarks, error) in
+            for place in placemarks!
+            {
+                
+                let annotation = MKPointAnnotation()
+                
+                annotation.coordinate = (place.location?.coordinate)!
+                annotation.title = place.name
+                self.mapView.addAnnotation(annotation)
+                
 
+            }
+        }
+        
+       
+    
     }
 }
 
